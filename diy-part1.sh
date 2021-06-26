82dc750c3ab52a3bcc86fc6da0ad75cc921d0063
yangchangjing <86527022+yangchangjing@users.noreply.github.com>
2021-06-27 00:29:06 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 sed -i '$a src-git liuran001_packages https://github.com/liuran001/openwrt-packages' feeds.conf.default
-sed -i '$a src-git https://github.com/jefferyto/openwrt-slide-switch.git' feeds.conf.default
+sed -i '$a src-git jefferyto https://github.com/jefferyto/openwrt-slide-switch.git' feeds.conf.default
